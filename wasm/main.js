ce8ea8213d0a138ded6c240b8fb1bdebca8206cd
Tugdual <tugdualgaillard@gmail.com>
2024-09-30 03:17:40 +0200

    choose file

--- wasm/main.js
+++ wasm/main.js
@@ -47,81 +47,94 @@ Module.onRuntimeInitialized = async () => {
         return arr;
     }
 
-    const resp = await fetch("test_data.txt");
-    if (!resp.ok) {
-      throw new Error(`Response status: ${resp.status}`);
-    }
-    const txtArray = createCharArray(await resp.bytes());
+    async function analyse(file){
 
-    const t_ptr = createPointerArray(1);
-    const h_ptr = createPointerArray(1);
+        // const txtArray = createCharArray(await resp.bytes());
+        //
+        const txtArray = createCharArray(await file.bytes());
 
-    const n_pts = Module._readData(txtArray.byteOffset, txtArray.byteLength,
-                     t_ptr.byteOffset, h_ptr.byteOffset);
-    const t = new Float64Array(memory.buffer, t_ptr[0], n_pts);
-    const h = new Float64Array(memory.buffer, h_ptr[0], n_pts);
+        const t_ptr = createPointerArray(1);
+        const h_ptr = createPointerArray(1);
 
-    let h_mean = 0.0;
-    h.forEach((x)=> h_mean += x);
-    h_mean /= h.length;
-    h.map((x)=> x-h_mean);
+        const n_pts = Module._readData(txtArray.byteOffset, txtArray.byteLength,
+                        t_ptr.byteOffset, h_ptr.byteOffset);
+        const t = new Float64Array(memory.buffer, t_ptr[0], n_pts);
+        const h = new Float64Array(memory.buffer, h_ptr[0], n_pts);
 
-
-
-    let pulsations = createF64Array(pulsations0.length);
-    pulsations.set(pulsations0);
-
-    let amplitudes = createF64Array(pulsations0.length);
-
-    let phases = createF64Array(pulsations0.length);
-
-    Module._getHarmonics(t.byteOffset, h.byteOffset, t.length, pulsations.byteOffset,
-                         h_mean, phases.byteOffset, amplitudes.byteOffset, pulsations.length);
+        let h_mean = 0.0;
+        h.forEach((x)=> h_mean += x);
+        h_mean /= h.length;
+        h.map((x)=> x-h_mean);
 
 
-    let htmlList = "";
-    for (let i = 0; i< comp_names.length; ++i){
-        htmlList += "<tr>\n"
-        htmlList += `<td>  ${comp_names[i]}  </td>\n`;
-        htmlList += `<td>  ${pulsations0[i]}  </td>\n`;
-        htmlList += `<td>  ${amplitudes[i]}  </td>\n`;
-        htmlList += `<td>  ${phases[i]}  </td>\n`;
-        htmlList += "</tr>\n"
-    }
-    document.getElementById('components').innerHTML = htmlList;
 
-    const t_fit = createF64Array(t.length * 4);
-    const h_fit = createF64Array(t.length * 4);
-    {
-        const dt = (t[t.length - 1] - t[0]) / t_fit.length ;
-        for (let i = 0; i<t_fit.length; ++i){
-            t_fit[i] = i * dt;
+        let pulsations = createF64Array(pulsations0.length);
+        pulsations.set(pulsations0);
+
+        let amplitudes = createF64Array(pulsations0.length);
+
+        let phases = createF64Array(pulsations0.length);
+
+        Module._getHarmonics(t.byteOffset, h.byteOffset, t.length, pulsations.byteOffset,
+                            h_mean, phases.byteOffset, amplitudes.byteOffset, pulsations.length);
+
+
+        let htmlList = "";
+        for (let i = 0; i< comp_names.length; ++i){
+            htmlList += "<tr>\n"
+            htmlList += `<td>  ${comp_names[i]}  </td>\n`;
+            htmlList += `<td>  ${pulsations0[i]}  </td>\n`;
+            htmlList += `<td>  ${amplitudes[i]}  </td>\n`;
+            htmlList += `<td>  ${phases[i]}  </td>\n`;
+            htmlList += "</tr>\n"
         }
+        document.getElementById('components').innerHTML = htmlList;
+
+        const t_fit = createF64Array(t.length * 4);
+        const h_fit = createF64Array(t.length * 4);
+        {
+            const dt = (t[t.length - 1] - t[0]) / t_fit.length ;
+            for (let i = 0; i<t_fit.length; ++i){
+                t_fit[i] = i * dt;
+            }
+        }
+
+        Module._sumHarmonics(t_fit.byteOffset, t_fit.length, pulsations.byteOffset, phases.byteOffset,
+                            amplitudes.byteOffset, h_mean, phases.length, h_fit.byteOffset);
+
+        var trace0 = {
+            x: t,
+            y: h,
+            mode: 'lines+markers',
+            name: 'data',
+        };
+
+        var trace1 = {
+            x: t_fit,
+            y: h_fit,
+            mode: 'lines',
+            name: 'fit',
+        };
+
+        var layout = {
+            title: 'Least square fitting',
+            xaxis: {title: 't (h)'},
+            yaxis: {title: 'h (m)'},
+        };
+
+        Plotly.newPlot( "graph", [trace0, trace1], layout);
     }
 
-    Module._sumHarmonics(t_fit.byteOffset, t_fit.length, pulsations.byteOffset, phases.byteOffset,
-                        amplitudes.byteOffset, h_mean, phases.length, h_fit.byteOffset);
+    const file0 = await fetch("test_data.txt");
+    if (!file0.ok) {
+    throw new Error(`Response status: ${resp.status}`);
+    }
+    analyse(file0);
 
-    var trace0 = {
-        x: t,
-        y: h,
-        mode: 'lines+markers',
-        name: 'data',
-    };
-
-    var trace1 = {
-        x: t_fit,
-        y: h_fit,
-        mode: 'lines',
-        name: 'fit',
-    };
-
-    var layout = {
-        title: 'Least square fitting',
-        xaxis: {title: 't (h)'},
-        yaxis: {title: 'h (m)'},
-    };
-
-    Plotly.newPlot( "graph", [trace0, trace1], layout);
-
+    const input = document.getElementById('inselec');
+    input.onchange = e => {
+        // getting a hold of the file reference
+        const file = e.target.files[0];
+        analyse(file);
+    }
 }
